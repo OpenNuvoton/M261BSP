9ab2e6fda427fb4d0a121de8c95c2ff1581e5274
SFWU <SFWU@nuvoton.com>
2019-09-27 11:08:31 +0800

    Add ISP_I2C sample code.
    Change-Id: Ic9e941704d0c0d99200fe6caa77af5449878497d

---
+++ SampleCode/ISP/ISP_I2C/main.c
@@ -0,0 +1,157 @@
+/****************************************************************************
+ * @file     main.c
+ * @version  V3.00
+ * @brief    Demonstrate how to update chip flash data through I2C interface
+ *           between chip I2C and ISP Tool.
+ *           Nuvoton NuMicro ISP Programming Tool is also required in this
+ *           sample code to connect with chip I2C and assign update file
+ *           of Flash.
+ *
+ * @note
+ * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "targetdev.h"
+#include "i2c_transfer.h"
+
+#define PLLCTL_SETTING      CLK_PLLCTL_128MHz_HIRC
+#define PLL_CLOCK           128000000
+
+
+
+uint32_t u32Pclk0;
+uint32_t u32Pclk1;
+
+void ProcessHardFault(void) {}
+void SH_Return(void) {}
+uint32_t CLK_GetPLLClockFreq(void)
+{
+    return 128000000;
+}
+
+uint32_t CLK_GetCPUFreq(void)
+{
+    return SystemCoreClock;
+}
+
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Enable HIRC clock */
+    CLK->PWRCTL |= CLK_PWRCTL_HIRCEN_Msk;
+
+    /* Wait for HIRC clock ready */
+    while(!(CLK->STATUS & CLK_STATUS_HIRCSTB_Msk));
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK->PLLCTL = CLK_PLLCTL_128MHz_HIRC;
+
+    while(!(CLK->STATUS & CLK_STATUS_PLLSTB_Msk));
+
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLKSEL_Msk)) | CLK_CLKSEL0_HCLKSEL_PLL;
+    CLK->CLKDIV0 = (CLK->CLKDIV0 & (~CLK_CLKDIV0_HCLKDIV_Msk)) | CLK_CLKDIV0_HCLK(2);
+
+    PllClock        = 128000000;
+    SystemCoreClock = 128000000 / 2;
+    CyclesPerUs     = SystemCoreClock / 1000000;  // For SYS_SysTickDelay()
+
+    /* Enable I2C1 peripheral clock */
+    CLK->APBCLK0 |= CLK_APBCLK0_I2C1CKEN_Msk;
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and CyclesPerUs automatically. */
+    SystemCoreClockUpdate();
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set PA multi-function pins for I2C1 SDA and SCL */
+    SYS->GPA_MFPL &= ~(SYS_GPA_MFPL_PA2MFP_Msk | SYS_GPA_MFPL_PA3MFP_Msk);
+    SYS->GPA_MFPL |= (SYS_GPA_MFPL_PA2MFP_I2C1_SDA | SYS_GPA_MFPL_PA3MFP_I2C1_SCL);
+
+    /* I2C clock pin enable schmitt trigger */
+    PA->SMTEN |= GPIO_SMTEN_SMTEN3_Msk;
+}
+
+int main(void)
+{
+    uint32_t au8CmdBuff[16];
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Enable ISP */
+    CLK->AHBCLK |= CLK_AHBCLK_ISPCKEN_Msk;
+    FMC->ISPCTL |= (FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_APUEN_Msk);
+
+    /* Get APROM size, data flash size and address */
+    g_apromSize = BL_EnableFMC();
+    g_dataFlashAddr = SCU->FNSADDR;
+
+    if (g_dataFlashAddr < g_apromSize) {
+        g_dataFlashSize = (g_apromSize - g_dataFlashAddr);
+    } else {
+        g_dataFlashSize = 0;
+    }
+
+    /* Init I2C */
+    I2C_Init();
+
+    /* Set Systick time-out as 300ms */
+    SysTick->LOAD = 300000 * CyclesPerUs;
+    SysTick->VAL   = (0x00);
+    SysTick->CTRL = SysTick->CTRL | SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
+
+    /* Wait for CMD_CONNECT command until Systick time-out */
+    while (1)
+    {
+        /* Wait for CMD_CONNECT command */
+        if (u8I2cDataReady == 1)
+        {
+            goto _ISP;
+        }
+        /* Systick time-out, go to APROM */
+        if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)
+        {
+            goto _APROM;
+        }
+    }
+
+_ISP:
+
+    /* Parse command from master and send response back */
+    while (1)
+    {
+        if (u8I2cDataReady == 1)
+        {
+            /* Get command from I2C receive buffer */
+            memcpy(au8CmdBuff, au8I2cRcvBuf, 64);
+            u8I2cDataReady = 0;
+            /* Parse the current command */
+            ParseCmd((unsigned char *)au8CmdBuff, 64);
+        }
+    }
+
+_APROM:
+
+    /* Reset system and boot from APROM */
+    SYS->RSTSTS = (SYS_RSTSTS_PORF_Msk | SYS_RSTSTS_PINRF_Msk);
+    FMC->ISPCTL &= ~(FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_BS_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+
+    /* Trap the CPU */
+    while (1);
+}
