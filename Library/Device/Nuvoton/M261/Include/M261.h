8a2f3fd4cd3dfdb4d22ba81b519da7e394fd43a2
YTLu <YTLu@nuvoton.com>
2023-08-29 11:12:44 +0800

    Modify Readme, Revision history and chm files.
    Change-Id: Ia32c5c610a5dffeba30d32c36dc32f041b9afac1

--- Library/Device/Nuvoton/M261/Include/M261.h
+++ Library/Device/Nuvoton/M261/Include/M261.h
@@ -28,7 +28,7 @@
   *
  * SPDX-License-Identifier: Apache-2.0
  *
-  * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+  * Copyright (C) 2019~2023 Nuvoton Technology Corp. All rights reserved.
   */
 
 
